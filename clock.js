18953cb6dd7cd3eca17321f36f1ec58af59565ab
SaralynOgden <Saralyn.Ogden@gmail.com>
2016-10-11 09:21:54 -0700

    turned on actual API querying

--- clock.js
+++ clock.js
@@ -2,12 +2,8 @@ const CronJob = require('cron').CronJob;
 const bot = require('./bot.js');
 const http = require("http");
 
-new CronJob('*/01 * * * * *',
+new CronJob('00 */01 * * * *',
   bot.start,
   null,
   true,
   'America/Los_Angeles');
-
-// setInterval(function() {
-//   http.get("http://chase.herokuapp.com");
-// }, 300000);
